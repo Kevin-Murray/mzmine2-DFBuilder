0873b2093cb774ee5659b313579f3a8559fab50e
murra668 <murra668@umn.edu>
2020-11-05 08:13:43 -0600

    Added MZmine copyright and license information

--- src/main/java/net/sf/mzmine/modules/rawdatamethods/peakpicking/diagnosticfilter/ExclusionInformation.java
+++ src/main/java/net/sf/mzmine/modules/rawdatamethods/peakpicking/diagnosticfilter/ExclusionInformation.java
@@ -1,7 +1,19 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2006-2020 The MZmine Development Team
+ * 
+ * This file is part of MZmine.
+ * 
+ * MZmine is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with MZmine; if not,
+ * write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+ * USA
  */
 package net.sf.mzmine.modules.rawdatamethods.peakpicking.diagnosticfilter;
 
